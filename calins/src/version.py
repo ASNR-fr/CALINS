63960091b90f565e39fefb9175b1d235f9cbcb6c
bpetillon <barthelemi.petillon@asnr.fr>
2026-05-27 15:56:03 +0200

    CORR : mkdocs formulas screening

--- calins/src/version.py
+++ calins/src/version.py
@@ -1,2 +1,2 @@
 """Version file - automatically generated."""
-__version__ = "1.0.0b0.dev0+41b3579.20260522"
+__version__ = "1.0.0b0.dev0+9e26e7f.20260527"
--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -12,5 +12,16 @@ window.MathJax = {
 };
 
 document$.subscribe(() => {
-  MathJax.typesetPromise()
+  // Wait for MathJax to be fully initialized
+  if (window.MathJax) {
+    if (MathJax.startup && MathJax.startup.promise) {
+      MathJax.startup.promise
+        .then(() => MathJax.typesetPromise())
+        .catch((err) => console.log('MathJax error:', err));
+    } else if (MathJax.typesetPromise) {
+      // Fallback for older versions
+      MathJax.typesetPromise()
+        .catch((err) => console.log('MathJax error:', err));
+    }
+  }
 })
